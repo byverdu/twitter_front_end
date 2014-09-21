3d204d34ba349da4dd3fa1d4b886d21e2ebc4b46
byverdu <byverdu@gmail.com>
2014-09-21 16:01:03 +0100

    countdown words finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,5 +30,26 @@ $container.masonry({
           });
       }
   });
+
+
+  $('.modal_main textarea').on('keydown',function(){
+
+    var count       = $(this).val().length+1,
+    
+    total_count     = $('.modal_footer .total_count'),
+        
+    actual_count    = 140-count;
+
+    total_count.text(actual_count);
+
+    if(actual_count<0) total_count.css('color', 'red');
+
+
+    // console.log('count',count)
+    // console.log('total count',total_count)
+    // //console.log('total_count_val',total_count_val)
+    // console.log('actual_count',actual_count)
+  })
+
 });
 
