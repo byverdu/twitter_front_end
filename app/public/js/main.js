1cf61270887c058260bee85b2bda563d0d00dc49
byverdu <byverdu@gmail.com>
2016-01-25 23:09:34 +0000

    added count tweet words functionality

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -4,6 +4,11 @@ document.addEventListener( 'DOMContentLoaded', () => {
 
   console.log('DOMContentLoaded event fired');
 
+  // Global variables
+  let textArea;
+  let textAreaBodyIndex;
+  let hidedButtons;
+
   /**
    * infinite scrolling
    **/
@@ -15,10 +20,11 @@ document.addEventListener( 'DOMContentLoaded', () => {
     let newNode = document.createElement( 'div' );
     let referenceNode = document.querySelectorAll( '.twitterFeeds__tweet' );
     let nodeToAppend = referenceNode[referenceNode.length - 1].nextSibling;
+
     newNode.setAttribute( 'class', 'secondBatchTweets' );
     newNode.innerHTML = this.responseText ;
 
-    parentNode.insertBefore(newNode, nodeToAppend);
+    parentNode.insertBefore( newNode, nodeToAppend );
   }
 
   // XMLHttpRequest to fetch second batch of data.
@@ -32,31 +38,68 @@ document.addEventListener( 'DOMContentLoaded', () => {
   // Loading infinite scroll
   function loadInfiniteScroll () {
     let bodyElement = document.querySelector('body');
+    let allTweets = document.querySelectorAll( '.twitterFeeds__tweet' );
+    let bodyScrollTop = bodyElement.scrollTop;
+    let lasTweetHeight = allTweets[ allTweets.length - 1 ].clientHeight;
 
-    if ( (bodyElement.scrollHeight - bodyElement.scrollTop) <= bodyElement.clientHeight ) {
-      doHttpRequest( );
+    if ( bodyScrollTop >= lasTweetHeight && bodyScrollTop <= (lasTweetHeight + 100) ) {
+      doHttpRequest();
     }
   }
 
   /**
-   * Textarea count
+   * Textarea tweet character count
    **/
-  let textarea = Array.from(document.querySelectorAll('.textareaCount'));
+  textArea = Array.from(document.querySelectorAll('.textareaSection-js'));
 
   function getTweetWordCount () {
 
     let maxCount = 140;
+    let currentCount = this.value.length;
+    let newCount = ( maxCount - currentCount );
+    let countSelector = this.parentNode.parentNode.querySelector( '.textareaCount-js' );
+    let buttonSelector = this.parentNode.parentNode.querySelector( '.textareaButton-js' );
+    console.log(currentCount, newCount);
 
-    console.log(this.value.length);
+    countSelector.innerHTML = newCount;
+
+    if ( currentCount > 0) {
+      buttonSelector.removeAttribute('disabled');
+    } else {
+      buttonSelector.setAttribute( 'disabled', 'enabled');
+    }
+
+    if ( newCount <= 10 ) {
+      countSelector.classList.add('textareaCountRed');
+    } else {
+      countSelector.classList.remove('textareaCountRed');
+    }
   }
 
-  textarea.forEach( (el) => {
-    el.addEventListener( 'keyup', getTweetWordCount );
-  });
+  textAreaBodyIndex = document.querySelector( '.twitterNewTweet__wrapper--input textarea' );
+  hidedButtons = document.querySelector('.twitterNewTweet__wrapper--body');
+
+
+  function removeHideClass () {
+    textAreaBodyIndex.style.height = '80px'; // bad !!!
+    hidedButtons.classList.remove( 'hide' );
+  }
+
+  function addHideClass () {
+    if ( textAreaBodyIndex.value.length === 0 ) {
+      textAreaBodyIndex.style.height = '35px';
+      hidedButtons.classList.add( 'hide' );
+    }
+  }
 
 
   // Adding event listeners to elements
 
+  textArea.forEach( (el) => {
+    el.addEventListener( 'keyup', getTweetWordCount );
+  });
   document.addEventListener( 'scroll', loadInfiniteScroll );
+  textAreaBodyIndex.addEventListener( 'focus', removeHideClass );
+  textAreaBodyIndex.addEventListener( 'blur', addHideClass );
 
 } );
