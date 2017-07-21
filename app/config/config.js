9680f6473196e11a663a78101c83b3ce321936dc
albert Byverdu :) <byverdu@gmail.com>
2017-07-21 01:34:01 +0100

    tokens updated

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // twitterAPI config values
   twitterAPI: {
-    consumer_key:    process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token:    process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+    consumer_key:    process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token:    process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
   },
 
   // API queries values
